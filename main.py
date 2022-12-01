ef84f1c1bcf6dd283f6afc8f15802e5782166e2b
Nestor <nestorrs2017@gmail.com>
2022-12-01 01:34:12 -0500

    cambios en las neuronas y metodos

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 from data import *
 from perseptron import *
 
-neuralNetwork = []
 
-for i in range(len(equipoID)):
-    juegos=[]
-    for j in range(len(equipoID[i])):
-        for k in range(j+1,len(equipoID[i])):
-            jugadores=[equipoID[i][j],equipoID[i][k]]
-            pesos=[ranking[i][j],ranking[i][k]]
-            juegos.append(perseptron(jugadores,pesos))
-    neuralNetwork.append(juegos)
-            #print("%i - %i"%(equipoID[i][j],equipoID[i][k]))
-    #print("\n")
-
-for i in range(len(neuralNetwork)):
-    for j in range(len(neuralNetwork[i])):
-        print(neuralNetwork[i][j].evalution())
-    print()
+'''
+for i in range(len(ranking)):
+    for j in range(len(ranking[i])):
+        for k in range(j+1,len(ranking[i])):
+'''            
--- perseptron.py
+++ perseptron.py
@@ -3,20 +3,25 @@ import math
 class perseptron():
     input=[]
     weight=[]
+    points=[]
     def __init__(self,input,weight):
         self.input=input
         self.weight=weight
+        self.points=[0,0,0,0]
 
-    def activation(self,x):
-        if  math.sin(x)>0.5:
-            return 1
-        elif math.sin(x)<-0.5:
-            return -1
-        else:
-            return 0
-
-    def evalution(self):
-        data=0
+    def jugarPartidos(self):
         for i in range(len(self.input)):
-            data+=self.input[i]*self.weight[i]
-        return self.activation(data)
+            for j in range(i+1,len(self.input)):
+                playerA=abs(math.sin(self.input[i]*self.weight[i]))
+                playerB=abs(math.sin(self.input[j]*self.weight[j]))
+                print("%f - %f"%(playerA,playerB))
+
+                
+
+inp=[1440,1464,1584,1694]
+pesos=[1,1,1,1]
+
+a=perseptron(inp,pesos)
+a.jugarPartidos()
+
+#print("%i - %i"%(i,j))
